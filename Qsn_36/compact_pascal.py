6eaf13fb12b19a843f0a18c3ed8b8c8ce97c9383
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2020-11-19 23:30:34 +0530

    Create compact_pascal.py

---
+++ Qsn_36/compact_pascal.py
@@ -0,0 +1,6 @@
+def compact_pascal(n,r=[]):
+    for x in range(n):
+        l = len(r)
+        r = [1 if i == 0 or i == l else r[i-1]+r[i] for i in range(l+1)]
+        yield r
+print(list(compact_pascal(8))
